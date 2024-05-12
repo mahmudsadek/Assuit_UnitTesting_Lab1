f91f2dad44763620d0a85f75c61d44199e1031c0
mahmoudsadek <mahmoudsadek.1905@gmail.com>
2024-05-12 14:27:57 +0300

    CarFactory

---
+++ CarFactoryLibrary_test2/CarFactoryTests.cs
@@ -0,0 +1,57 @@
+﻿using CarFactoryLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFactoryLibrary_test2
+{
+    public class CarFactoryTests
+    {
+        [Fact]
+        public void When_NewCar_CarTypeAudi_null()
+        {
+            // Arrange
+            BMW BMW = new BMW();
+
+            // Act
+            Car car = BMW.GetMyCar();
+
+            // Assert
+            Assert.NotNull(car);
+        }
+
+        [Fact]
+        public void NewCar_CarTypeBMW_BMW()
+        {
+            // Arrange
+
+            // Act
+            Car? car = CarFactory.NewCar(CarTypes.BMW);
+
+            // Reference Asserts
+            Assert.NotNull(car);
+
+            // Type Asserts
+            Assert.IsType<BMW>(car);
+            Assert.IsAssignableFrom<Car>(new BMW());
+        }
+
+        [Fact]
+        public void NewCar_CarTypeBMW_Exception()
+        {
+            // Arrange
+
+
+
+            // Assert
+            Assert.Throws<NotImplementedException>(() =>
+            {
+                // Act
+                Car? result = CarFactory.NewCar(CarTypes.BMW);
+            });
+        }
+
+    }
+}
